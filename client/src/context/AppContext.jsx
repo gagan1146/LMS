365c76b53b465fdb5394c662a364b4ae1a5b82f2
gagan1146 <gagansharma99690@gmail.com>
2025-03-31 20:43:25 +0530

    Adding basic Functionalities

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,17 +1,24 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
 
 const AppContext = createContext();
 
-export const AppContextProvider =(props)=>{
-    const value={
+export const AppContextProvider = (props) => {
+  const currency = import.meta.env.VITE_CURRENCY;
+  const [allCourses, setAllCourses] = useState([]);
 
-    }; 
-    return (
-        <AppContext.Provider value ={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  const fetchAllCourses = () => {
+    console.log("Fetching courses:", dummyCourses);
+    setAllCourses(dummyCourses); // Correct way to update state
+  };
 
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
+
+  const value = { currency, allCourses };
+
+  return <AppContext.Provider value={value}>{props.children}</AppContext.Provider>;
+};
 
 export default AppContext;
